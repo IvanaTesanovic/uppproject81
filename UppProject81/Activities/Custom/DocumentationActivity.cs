5b2b14cc553dcbf845f55e652fb9debc3f524664
ivana57 <inna0507@gmail.com>
2017-10-09 23:11:56 +0200

    Initial commit with project

---
+++ UppProject81/Activities/Custom/DocumentationActivity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+
+namespace Activities.Custom
+{
+    public sealed class DocumentationActivity : CodeActivity
+    {
+        public OutArgument<bool> IsDocumentationFiled { get; set; }
+
+        // If your activity returns a value, derive from CodeActivity<TResult>
+        // and return the value from the Execute method.
+        protected override void Execute(CodeActivityContext context)
+        {
+            //context.SetValue(IsDocumentationFiled, false);
+            IsDocumentationFiled.Set(context, true);
+        }
+    }
+}
---
+++ UppProject81/UppProject81/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+using Activities.Model;
+
+namespace UppProject81
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Activity process = new UppProcess();
+            //WorkflowInvoker.Invoke(process);
+
+            Activity testing = new DocumentationAndReportActivity();
+            WorkflowInvoker.Invoke(testing);
+
+            Console.ReadLine();
+        }
+    }
+}
